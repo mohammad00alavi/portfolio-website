a56dbf7660d3570646876f4ed7473db2d76b7e6a
Mohammad Alavi <43592603+Muovax@users.noreply.github.com>
2019-08-07 13:02:32 +0430

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#js--type", {
+    strings: ["The Web Developer ^300", "The Web Designer ^300", "The Photographer ^300"],
+    showCursor: false,
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    smartBackspace: false // Default value
+  });
